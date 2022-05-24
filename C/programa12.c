b76650490b9c6e0908f1aa0875820be40400fad3
TedUrc <arthur.esteves2000@gmail.com>
2022-05-24 00:39:45 -0300

    Código C

--- C/programa12.c
+++ C/programa12.c
@@ -13,30 +13,38 @@ struct vagao
 
 vagao *primeiro;
 
-int main()
+vagao *inserir_formulario(vagao *referencia, int numeroVg, char *produtoNome)
 {
-    vagao v1, v2, v3;
-    _Bool continua = 1, imprime = 1;
-    int numVagao;
+    vagao *novoVagao = (vagao *)malloc(sizeof(vagao));
+    novoVagao->produtoVagao = (char *)malloc(sizeof(char));
 
-    v1.anteriorVagao = NULL;
-    v1.numeroVagao = 1;
-    strcpy(v1.produtoVagao, "Ferro");
-    v1.proximoVagao = &v2;
+    if(referencia == NULL)
+    {
+        novoVagao->anteriorVagao = NULL;
+        novoVagao->numeroVagao = numeroVg;
+        strcpy(novoVagao->produtoVagao, produtoNome);
+        novoVagao->proximoVagao = NULL;
+        return novoVagao;
+    }
+    else
+    {
+        while(referencia->proximoVagao != NULL)
+        {
+            referencia = referencia->proximoVagao;
+        }
+        novoVagao->anteriorVagao = referencia;
+        novoVagao->numeroVagao = numeroVg;
+        strcpy(novoVagao->produtoVagao, produtoNome);
+        novoVagao->proximoVagao = NULL;
+        referencia->proximoVagao = novoVagao;
+    }
+}
 
-    v2.anteriorVagao = &v1;
-    v2.numeroVagao = 2;
-    strcpy(v2.produtoVagao, "Bauxita");
-    v2.proximoVagao = &v3;
 
-    v3.anteriorVagao = &v2;
-    v3.numeroVagao = 3;
-    strcpy(v3.produtoVagao, "Algodao");
-    v3.proximoVagao = NULL;
+void informe_vagao(int numVagao)
+{
+    _Bool continua = 1, retorne = 1;
 
-    primeiro = &v1;
-
-    numVagao = 2;
     while(continua == 1)
     {
         if(primeiro == NULL)
@@ -45,24 +53,13 @@ int main()
         }
         else
         {
-            if(numVagao == primeiro->numeroVagao && imprime == 1)
+            if(numVagao == primeiro->numeroVagao && retorne == 1)
             {
-                printf("Numero do Vagao: %d\n", primeiro->numeroVagao);
-                printf("Nome do Produto: %s\n\n", primeiro->produtoVagao);
+                printf("Numero Vagao: %d\n", primeiro->numeroVagao);
+                printf("Nome do Produto: %s\n", primeiro->produtoVagao);
             }
 
-            if(imprime == 0)
-            {
-                if(primeiro->anteriorVagao == NULL)
-                {
-                    continua = 0;
-                }
-                else
-                {
-                    primeiro = primeiro->anteriorVagao;
-                }
-            }
-            else
+            if(retorne == 1)
             {
                 if(primeiro->proximoVagao != NULL)
                 {
@@ -70,12 +67,30 @@ int main()
                 }
                 else
                 {
-                    imprime = 0;
+                    retorne = 0;
                 }
             }
-
+            else
+            {
+                if(primeiro->anteriorVagao == NULL)
+                {
+                        continua = 0;
+                }
+                else
+                {
+                    primeiro = primeiro->anteriorVagao;
+                }
+            }
         }
     }
+}
 
+int main()
+{
+    primeiro = inserir_formulario(primeiro, 1, "Ferro");
+    inserir_formulario(primeiro, 2, "Bauxita");
+    inserir_formulario(primeiro, 3, "Malaquita");
+
+    informe_vagao(2);
     return 0;
 }
